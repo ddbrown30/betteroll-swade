3ce12c0f8c82ba439f88de9cacda654b31225295
Dan Brown <danbrown@backtablestudios.com>
2025-06-12 16:59:39 -0700

    GM actions no longer apply mods
    GM actions were already changed to instead activate other normal actions so this was causing them to apply twice.

--- betterrolls-swade2/scripts/attribute_card.js
+++ betterrolls-swade2/scripts/attribute_card.js
@@ -12,7 +12,6 @@ import {
   process_common_actions,
 } from "./cards_common.js";
 import { run_macros } from "./item_card.js";
-import { get_enabled_gm_actions } from "./gm_actions.js";
 import { BrCommonCard } from "./BrCommonCard.js";
 
 /**
@@ -166,9 +165,6 @@ export async function roll_attribute(br_card, expend_bennie) {
   const macros = [];
   for (const action of br_card.get_selected_actions()) {
     process_common_actions(action.code, extra_data, macros, br_card.actor);
-  }
-  for (const action of get_enabled_gm_actions()) {
-    process_common_actions(action, extra_data, macros, br_card.actor);
   }
   if (expend_bennie) {
     await spend_bennie(br_card.actor);
